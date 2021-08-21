a80b95c2479976ff21f7ccf68d9a09ca4c103c88
advaitb <adsgarfield95@gmail.com>
2021-08-21 11:58:45 -0500

    add files

---
+++ cmake-build-release/CMakeFiles/stew.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stew.dir/src/city.c.o"
+  "CMakeFiles/stew.dir/src/hll.c.o"
+  "CMakeFiles/stew.dir/src/log.c.o"
+  "CMakeFiles/stew.dir/src/main.c.o"
+  "stew"
+  "stew.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/stew.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
